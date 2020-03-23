626447cfc9b5435da58a38ace557b2b989ff2d88
lebuhorskiy <lebuhorskiy65@gmail.com>
2020-03-24 00:31:48 +0200

    Remove lodash

--- src/main.js
+++ src/main.js
@@ -6,9 +6,6 @@ import router from './routes';
 
 Vue.use(VueRouter)
 
-import VueLodash from 'vue-lodash'
-import lodash from 'lodash'
-Vue.use(VueLodash, { name: 'custom' , lodash: lodash })
 
 Vue.config.productionTip = false
 
